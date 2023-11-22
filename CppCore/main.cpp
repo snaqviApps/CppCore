58a86cbe84bee84a0a910d4ce2acc203b01ebac4
sayyidnaqvi <naqvi.sdsu@gmail.com>
2023-11-22 04:43:40 -0600

    Cpp review of pointers

--- CppCore/main.cpp
+++ CppCore/main.cpp
@@ -7,8 +7,30 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hi, C++ World! ... it has been a while to visit you\n";
+    
+    int num1 = 11;
+    int num2 = num1;
+    
+    num1 = 22;
+    cout <<"num1: " << num1 << endl;
+    cout <<"num2: " << num2 << endl; // not using pointer still 'num2' is 11, though 'num1' is now '22'
+    
+    
+    /** now using Pointers */
+    cout << "\nnow with pointers\n" << endl;
+    int* num3 = new int(110);
+    int *num4 = num3;
+    
+    *num3 = 220;
+    
+    cout <<"num3 address: " << num3 << ", nme3 content: " << *num3 << endl;
+    cout <<"num4 address: " << num4 << ", nme4 content: " << *num4 << endl; // with pointers,'num4' is same
+    
     return 0;
 }
+
